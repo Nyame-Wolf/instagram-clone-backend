59ebf956193ccb678366e62aa520042d05f9d139
VitorGuedesMadeira <vgm_rox@hotmail.com>
2023-03-08 20:30:27 -0300

    Generate devise user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_one_attached :image
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
