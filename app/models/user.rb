aac5b4630f26257fe2c890b0916a8b543e637115
VitorGuedesMadeira <vgm_rox@hotmail.com>
2023-03-08 21:11:38 -0300

    Generate local master key and credentials.yml.enc
    Add jti to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+
+  devise :database_authenticatable, :registerable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: self
+
   has_one_attached :image
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
