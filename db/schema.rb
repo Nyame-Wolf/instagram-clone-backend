0e60c85461727fea8cc611884c5c5963e38d95f6
VitorGuedesMadeira <vgm_rox@hotmail.com>
2023-03-08 21:16:44 -0300

    Add respond_with using fast_jsonapi method

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_08_232643) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_09_000829) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -85,7 +85,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_08_232643) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "jti"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["jti"], name: "index_users_on_jti"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
