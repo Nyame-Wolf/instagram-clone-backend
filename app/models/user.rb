11545547d5940b815a4e342cf941fabef3fcd7b4
Nyame-Wolf <mumenyam@gmail.com>
2023-03-23 15:07:00 +0300

    fix permitted parameters

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,6 @@ class User < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true,
                    length: { maximum: 30, too_long: '%<count>s characters is the maximum allowed' }
+  validates :username, presence: true, uniqueness: true,
+                       length: { maximum: 30, too_long: '%<count>s characters is the maximum allowed' }
 end
